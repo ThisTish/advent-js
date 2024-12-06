61c54b611e03ed6d226086ebb4a81576fcba6bb9
Tish <tishthegoat@gmail.com>
2024-12-06 00:12:46 -0500

    almost there, need to look at the math and refrences

--- app/day3/page.tsx
+++ app/day3/page.tsx
@@ -3,19 +3,21 @@
 import React, { useRef, ChangeEvent, useState, useEffect } from "react";
 
 const DayThreePage = () => {
-	const [height, setHeight] = useState(100)
+	const [height, setHeight] = useState('100px')
 	const textAreaRef = useRef<HTMLTextAreaElement>(null)
 
-	useEffect(() =>{
-		if(textAreaRef.current){
-			if(textAreaRef.current.style.height > `${height}px`){
-			setHeight(parseInt(textAreaRef.current.style.height))
-		}
-	}
-	})
 
 
 	const handleChanges = (value: string, e: ChangeEvent<HTMLTextAreaElement>) => {
+		
+	useEffect(() =>{
+		if(textAreaRef.current){
+			if(textAreaRef.current.scrollHeight > parseInt(height)){
+			setHeight(textAreaRef.current.scrollHeight + 'px')
+		}
+	}
+	},[textAreaRef.current?.style.height])
+		
 	}
 	return (
 		<div className="aspect-video w-1/2 mx-auto bg-emerald-700 rounded-md border-red-600 border-4 ">
